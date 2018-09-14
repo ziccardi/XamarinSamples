ef0897722de3db082d366b9204494f867311e1f4
Massimiliano Ziccardi <mziccard@redhat.com>
2018-09-14 11:25:53 +0200

    First commit

---
+++ ProgressBarVisibility/ProgressBarTestAndroid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ProgressBarTestAndroid
+{
+    [Activity(Label = "ProgressBarTestAndroid", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            ProgressBar barOfProgression = FindViewById<ProgressBar>(Resource.Id.progressBarMap);
+            CheckBox checkBox = FindViewById<CheckBox>(Resource.Id.checkbox1);
+
+            checkBox.CheckedChange += (sender, evt) => {
+                barOfProgression.Visibility = evt.IsChecked ? Android.Views.ViewStates.Visible : Android.Views.ViewStates.Invisible;
+            };
+        }
+    }
+}
+
